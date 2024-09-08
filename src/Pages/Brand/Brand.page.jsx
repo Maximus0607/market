20a4ce8bcfe21f29938032296c6cf538785458c5
vijeyender <maxbussinesssolutions@gmail.com>
2024-09-08 12:02:33 +0530

    navigation

--- src/Pages/Brand/Brand.page.jsx
+++ src/Pages/Brand/Brand.page.jsx
@@ -36,7 +36,7 @@ export default function Brand() {
                     <h1 className="text-secondary-emphasis">Brand Details</h1>
                 </div>
                 <div className="col text-end align-self-center">
-                    <a className="btn btn-outline-info btn-lg" role="button" onClick={()=>navigate('brand/createbrand')}>Create New Brand</a></div>
+                    <a className="btn btn-outline-info btn-lg" role="button" onClick={()=>navigate('/createbrand')}>Create New Brand</a></div>
             </div>
             {responseStatus === "failed" ? <h1> Not able to fetch brand details</h1> :
                 <div className="row gy-4 row-cols-1 row-cols-sm-2 row-cols-lg-3 mb-3">
